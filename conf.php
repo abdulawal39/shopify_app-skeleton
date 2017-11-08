18633cc62aab6d4376e3d9e26ec5919f16c391b3
Abdul Awal <mamb2b.awal@gmail.com>
2017-11-09 04:29:48 +0600

    Added the Redirect URI Option

--- conf.php
+++ conf.php
@@ -1,4 +1,5 @@
 <?php
 
-	define('SHOPIFY_APP_API_KEY', '');
-	define('SHOPIFY_APP_SHARED_SECRET', '');
+	define('SHOPIFY_APP_API_KEY', ''); // API Key
+	define('SHOPIFY_APP_SHARED_SECRET', ''); // Shared Secret
+	define('REDIRECT_URL', ''); // put the url to oath.php file here
